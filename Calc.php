07646fcc2a2bb28c120b981c04fd70fd0485d414
Varal7 <varaal@gmail.com>
2016-06-22 15:27:13 +0200

    Be able to deal with multiple delimiters

--- Calc.php
+++ Calc.php
@@ -1,12 +1,23 @@
 <?php
 class Calc
 {
-   public function add($numbers) {
-     if ($numbers == "") {
+   public function add($string) {
+     if ($string == "") {
        return 0;
      }
      $result = 0;
-     $num_arr = explode(",", $numbers);
+     $lines = explode("\n", $string);
+     if (substr($lines[0], 0, 2) == "//") {
+       $delimiter = explode("/", $lines[0])[2];
+       $numbers = explode($delimiter, $lines[1]);
+       foreach ($numbers as $num) {
+         $result = $result + intval($num);
+       }
+       return $result;
+     }
+
+
+     $num_arr = explode(",", $string);
      foreach ($num_arr as $part) {
        $part_arr = explode("\n", $part);
        foreach ($part_arr as $num) {
