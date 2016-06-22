ad1531a3ec3b02e18f1d2b2e09ecb64f09532851
Varal7 <varaal@gmail.com>
2016-06-22 14:46:08 +0200

    Test for adding one number

--- CalcTest.php
+++ CalcTest.php
@@ -7,4 +7,9 @@ class CalcTest extends PHPUnit_Framework_TestCase {
         $result = $c->add("");
         $this->assertEquals($result, 0);
     }
+    public function testAddOneNumber() {
+        $c = new Calc();
+        $result = $c->add("1");
+        $this->assertEquals($result, 1);
+    }
 }
